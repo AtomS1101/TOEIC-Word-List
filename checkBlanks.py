ac96eea214d061d7c124a8c93e08ebda9edb8a36
Atom <121838254+AtomS1101@users.noreply.github.com>
2025-11-23 21:29:57 +0900

    Modify start/end indices and checkArea slice
    Updated the start and end indices for checking rows and adjusted the slice for checkArea to exclude the last element.

--- checkBlanks.py
+++ checkBlanks.py
@@ -1,8 +1,8 @@
 import csv
 
 class Setting:
-	start = 10
-	end = 51
+	start = 1
+	end = 300
 
 def main():
 	with open("words.csv", "r", encoding="utf-8") as fileW:
@@ -10,10 +10,10 @@ def main():
 		line = list(reader)
 		errors = []
 		for i, row in enumerate(line[Setting.start:Setting.end+1]):
-			checkArea = row[2: 7]
+			checkArea = row[2: 6]
 			noBlank = [n.replace(" ", "").replace("\u3000", "") for n in checkArea]
 			if not all(noBlank):
-				position = ["[word]", "[parts]", "[meaning]", "[example]", "[synonym]"]
+				position = ["[word]", "[parts]", "[meaning]", "[example]"]
 				errorCell = [j for j, cell in enumerate(checkArea) if not cell]
 				errors.append(f"{i+Setting.start}: {' '.join([position[j] for j in errorCell])}")
 	if errors:
