ec69eda403a1d91ec3c47c267a5edc53b1817d70
Atom <121838254+AtomS1101@users.noreply.github.com>
2026-01-22 13:17:26 +0900

    Modify end limit and improve duplicate detection
    Updated end limit and encoding for CSV file. Enhanced duplicate word detection by including index in output.

--- checkDouble.py
+++ checkDouble.py
@@ -2,11 +2,11 @@ import csv
 
 class Setting:
 	start = 1
-	end = 300
+	end = 600
 
 def main():
 	thirdColumn = []
-	with open("words.csv", "r", encoding="utf-8") as fileW:
+	with open("words.csv", "r", encoding="cp932") as fileW:
 		reader = csv.reader(fileW)
 		for row in reader:
 			if len(row) > 3:
@@ -14,13 +14,17 @@ def main():
 				thirdColumn.append(word)
 	thirdColumn = thirdColumn[Setting.start:Setting.end+1]
 	dic = {}
-	for word in thirdColumn:
+	flg = False
+	for i, word in enumerate(thirdColumn):
 		if word not in dic:
 			dic[word] = 1
 		else:
 			dic[word] += 1
-			print(word)
+			print(f"{i+Setting.start}: {word}")
+			flg = True
 
+	if not flg:
+		print("There are no duplicate words")
 
 if __name__ == "__main__":
 	main()
